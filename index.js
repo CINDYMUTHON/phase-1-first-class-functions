877d439629d6c3cf0b9d4f78c4335025d5e3adec
CINDY <git@github.com:CINDYMUTHON/challenge2.git>
2022-12-07 12:43:09 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,17 @@
+const receivesAFunction = function(callback){
+    callback();
+};
+receivesAFunction();
+
+function returnsANamedFunction(){
+    return function namedFn ()
+}
+    console. log("I am the returned named function");
+    {;
+    }
+
+    function returnsAnAnonymousFunction(){
+        return function()}
+        console. log("i am the returned anonymous function");
+    {;
+    }
